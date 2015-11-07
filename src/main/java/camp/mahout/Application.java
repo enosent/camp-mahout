0acd03d703c356422e8b1ae63abeddc3e424a7d7
seo il hyun <enosent7@gmail.com>
2015-11-07 15:19:21 +0900

    Initial commit.

---
+++ src/main/java/camp/mahout/Application.java
@@ -0,0 +1,16 @@
+package camp.mahout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Created by enosent on 2015. 11. 7..
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
